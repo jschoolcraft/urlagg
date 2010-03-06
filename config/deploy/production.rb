01b0ef1459c0251b0a3521e74719c15293a0706a
Jeff Schoolcraft <jeff@thequeue.net>
2010-03-06 08:55:23 -0500

    removing god now that we are using bluepill

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,21 +2,6 @@ set :host, "urlagg.com"
 set :deploy_to, "/var/www/apps/#{application}"
 set :branch, "master"
 
-# only have god running in production
-set :god_task, 'link_updater'
-namespace :deploy do
-  desc "Stop god"
-  task :before_deploy do
-    run "/opt/ruby-enterprise/bin/god stop #{god_task} && sleep 10"
-  end
-  
-  desc "Restarting god"
-  task :before_restart do
-    run "/opt/ruby-enterprise/bin/god load #{current_path}/config/link_updater.god"
-    run "/opt/ruby-enterprise/bin/god start #{god_task}"
-  end
-end
-
 # Bluepill related tasks
 # set user up in sudoers with: deploy ALL=(ALL) NOPASSWD: /opt/ruby-enterprise/bin/bluepill
 
