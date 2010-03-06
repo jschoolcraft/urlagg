65a510b372402256c0c676d2c2c1de0a3a69bd4d
Jeff Schoolcraft <jeff@thequeue.net>
2010-03-06 08:53:28 -0500

    moving from god to bluepill

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,3 +16,25 @@ namespace :deploy do
     run "/opt/ruby-enterprise/bin/god start #{god_task}"
   end
 end
+
+# Bluepill related tasks
+# set user up in sudoers with: deploy ALL=(ALL) NOPASSWD: /opt/ruby-enterprise/bin/bluepill
+
+after "deploy:update", "bluepill:quit", "bluepill:start"
+namespace :bluepill do
+  desc "Stop processes that bluepill is monitoring and quit bluepill"
+  task :quit, :roles => [:app] do
+    sudo "/opt/ruby-enterprise/bin/bluepill stop"
+    sudo "/opt/ruby-enterprise/bin/bluepill quit"
+  end
+ 
+  desc "Load bluepill configuration and start it"
+  task :start, :roles => [:app] do
+    sudo "/opt/ruby-enterprise/bin/bluepill load #{release_path}/config/production.pill"
+  end
+ 
+  desc "Prints bluepills monitored processes statuses"
+  task :status, :roles => [:app] do
+    sudo "/opt/ruby-enterprise/bin/bluepill status"
+  end
+end
