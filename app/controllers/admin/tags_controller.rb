b64d62247365f5e70beb79ab07f1358298c2f11e
Jeff Schoolcraft <jeff@thequeue.net>
2010-08-24 11:26:53 -0400

    killing admin dashboard

--- app/controllers/admin/tags_controller.rb
+++
@@ -1,8 +0,0 @@
-class Admin::TagsController < Admin::Base
-  # resource_controller
-  
-  protected
-    def collection
-      @collection ||= end_of_association_chain.paginate(:page => params[:page])
-    end
-end
--- app/controllers/admin/users_controller.rb
+++
@@ -1,8 +0,0 @@
-class Admin::UsersController < Admin::Base 
-  # resource_controller
-  
-  protected
-    def collection
-      @collection ||= end_of_association_chain.paginate(:page => params[:page])
-    end
-end
