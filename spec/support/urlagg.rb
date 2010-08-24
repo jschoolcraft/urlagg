2baa7e9c930d486fde8cc654d78fc7c29be1398b
Jeff Schoolcraft <jeff@thequeue.net>
2010-08-24 12:10:58 -0400

    Getting rid of the rest of the old super_admin stuff

--- spec/support/urlagg.rb
+++ spec/support/urlagg.rb
@@ -19,23 +19,5 @@ RSpec.configure do |config|
   end
 
   def user_session(stubs = {}, user_stubs = {})
-    @current_user_session ||= mock_model(UserSession, {:user => current_user(user_stubs)}.merge(stubs))
-  end
-
-  # super_user admin
-  def current_super_user(stubs = {})
-    @current_super_user ||= stub_model(SuperUser, stubs)
-  end
-
-  def super_user_session(stubs = {}, user_stubs = {})
-    @current_super_user_session ||= mock_model(SuperUserSession, {:super_user => current_super_user(user_stubs)}.merge(stubs))
-  end
-
-  def admin_login(session_stubs = {}, user_stubs = {})
-    SuperUserSession.stub!(:find).and_return(super_user_session(session_stubs, user_stubs))
-  end
-
-  def admin_logout
-    @super_user_session = nil
   end
 end
